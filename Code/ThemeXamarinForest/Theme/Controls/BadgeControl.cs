ec34185dd6fa9cd4e85f0d5d4213397df16374c3
ioadres <andres.garcia.dev@outlook.com>
2017-10-12 18:57:57 +0200

    Create badgeControl

---
+++ Code/ThemeXamarinForest/Theme/Controls/BadgeControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Theme.Controls
+{
+    public class CircleControl : Frame
+	{		
+		public static readonly BindableProperty ActiveProperty =
+			  BindableProperty.Create(nameof(Active), typeof(bool), typeof(CircleControl), false);
+
+		public bool Active
+		{
+			get { return (bool)GetValue(ActiveProperty); }
+			set { SetValue(ActiveProperty, value); }
+		}		
+
+		/// <summary>
+		/// The corner radius property.
+		/// </summary>
+		public static readonly BindableProperty RadiusProperty =
+			BindableProperty.Create("Radius", typeof(double), typeof(CircleControl), 0.0);
+
+		/// <summary>
+		/// Gets or sets the corner radius.
+		/// </summary>
+		public double Radius
+		{
+			get { return (double)GetValue(RadiusProperty); }
+			set
+			{
+
+				SetValue(RadiusProperty, value);
+			}
+		}
+
+		
+	}
+}
